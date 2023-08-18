9df0ab233e4a5109e531e71e458071c2f9d3285b
cuonpm <cphimanh@gmail.com>
2023-08-18 08:56:29 +0700

    Update user.sh

--- user.sh
+++ user.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 USER=systemadmin
+
+rpm -qa | grep sudo
+if [ $? -ne 0 ] ; then
+    export http_proxy=http://180.148.128.125:80/ && export https_proxy=http://180.148.128.125:80/ && yum install sudo -y && export http_proxy= && export https_proxy=
+fi
+
 useradd -m $USER -s /bin/bash && mkdir /home/$USER/.ssh/ && touch /home/$USER/.ssh/authorized_keys && chown -R $USER:$USER /home/$USER/.ssh && chmod 600 /home/$USER/.ssh/authorized_keys
 echo "$USER        ALL=(ALL)       NOPASSWD: ALL" >> /etc/sudoers
 su - $USER -c "echo 'ssh-rsa AAAAB3NzaC1yc2EAAAABJQAAAQEAiOY7BGJzuEvXCjPBtCMGWkGqiAklPxoCRxXkb1Sk9yAdGPIQGEMSO00CoSeNYsTkqrADIkXBF4rOcppU5iYwVtPKuCWq3FBNaWHHYPshDTKq4fdF70e+TL18xrJkGTfBENAk/f6Rx5mwH3Hmd30PGvQK124Auqlt/79QyU8/BFwWSepkUMGKVHb/8hDmgvq7mwh1G0KjXKuUC/vHrUWR/7rC7Km4R3kAct0ePQeEuQ+WeTpCuc8P5iLpFw+iWzicZrlGiRpLlCmt4eYsaoIb5OTZA4eqLLOxrw5m9jZPAZ1p3Pe4Jk9S4AZttqs3xedIheUL4XWhQzUaptCvdIkz0Q== system-admin' > /home/$USER/.ssh/authorized_keys"
